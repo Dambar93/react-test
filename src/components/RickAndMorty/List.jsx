35146ab03244c821d7982f9c4d9325b057b3847f
Dambar93 <97044258+Dambar93@users.noreply.github.com>
2022-07-19 22:58:51 +0300

    added table filter

--- src/components/RickAndMorty/List.jsx
+++ src/components/RickAndMorty/List.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
-import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 
@@ -11,6 +11,9 @@ const List = () => {
     const [list, setList] = useState([]);
     const [page,setPage] = useState(1);
     const [lastPage, setlastPage]= useState(1);
+    const [visible, setVisible]=useState(false);
+    const [check, setChecked]=useState({'id': true, 'name': true, "image": true, "status": true, "species": true, "type": true, "gender": true, "origin": true, "location": true})
+
 
     useEffect(() => {
 
@@ -23,36 +26,60 @@ const List = () => {
         fetchData()
     }, [page])
 
+    const clickHandler = () => {
+        if(visible){
+            setVisible(false)
+        } else if (!visible){
+            setVisible(true)
+        }
+    }
+    
+    const checkHandler = (name) => {
+        if(check[name]) {
+            let arr = {...check};
+            arr[name] = false;
+            setChecked({...arr})
+        } else if (!check[name]){
+            let arr = {...check};
+            arr[name] = true;
+            setChecked({...arr})
+        }
+        
+    }
+
     return (
         <div>
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-            </Dropdown.Toggle>
+            <div style={{textAlign: "right"}}>
+                <button onClick={()=> clickHandler()}>Filter </button>
+                <ul style={{display: visible ? "block" : "none", listStyleType: "none"}}>
+                    <li><input type="checkbox" defaultChecked={check["id"] }  onClick={() => {checkHandler("id")}}/>ID</li>
+                    <li><input type="checkbox" defaultChecked={check["image"] }  onClick={() => {checkHandler("image")}}/>Image</li>
+                    <li><input type="checkbox" defaultChecked={check["name"] }  onClick={() => {checkHandler("name")}}/>Name</li>
+                    <li><input type="checkbox" defaultChecked={check["status"] }  onClick={() => {checkHandler("status")}}/>Status</li>
+                    <li><input type="checkbox" defaultChecked={check["species"] }  onClick={() => {checkHandler("species")}}/>Species</li>
+                    <li><input type="checkbox" defaultChecked={check["type"] }  onClick={() => {checkHandler("type")}}/>Type</li>
+                    <li><input type="checkbox" defaultChecked={check["gender"] }  onClick={() => {checkHandler("gender")}}/>Gender</li>
+                    <li><input type="checkbox" defaultChecked={check["origin"] }  onClick={() => {checkHandler("origin")}}/>Origin</li>
+                    <li><input type="checkbox" defaultChecked={check["location"] }  onClick={() => {checkHandler("location")}}/>Location</li>
+                    {console.log(check)}
 
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1" onClick={() => "unchecked"}>
-                    <input type="checkbox" class="custom-control-input" id="checkbox1" checked onClick={() => "unchecked"}/>
-                    <label class="custom-control-label" for="checkbox1" >Check me</label>                    
-                </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
+                </ul>
+            </div>
+            
 
         <Table striped bordered hover >
             
             <thead style={{position: "sticky", top: "0", background:"white"}}>
                 <tr >
-                    <th>ID</th>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Status</th>
-                    <th>Species</th>
-                    <th>Type</th>
-                    <th>Gender</th>
-                    <th>Origin</th>
-                    <th>Location</th>
+                    <th style={{display: check["id"] ? "table-cell" : "none"}}>ID</th>
+                    <th style={{display: check["image"] ? "table-cell" : "none"}}>Image</th>
+                    <th style={{display: check["name"] ? "table-cell" : "none"}}>Name</th>
+                    <th style={{display: check["status"] ? "table-cell" : "none"}}>Status</th>
+                    <th style={{display: check["species"] ? "table-cell" : "none"}}>Species</th>
+                    <th style={{display: check["type"] ? "table-cell" : "none"}}>Type</th>
+                    <th style={{display: check["gender"] ? "table-cell" : "none"}}>Gender</th>
+                    <th style={{display: check["origin"] ? "table-cell" : "none"}}>Origin</th>
+                    <th style={{display: check["location"] ? "table-cell" : "none"}}>Location</th>
                     
 
                 </tr>
@@ -61,15 +88,15 @@ const List = () => {
                 
                     {list.map((person,index) => {
 
-                            return(<tr><td>{person.id}</td>
-                                        <td><img src={person.image} alt="" style={{width: "100px", height: "100px"}}/></td>
-                                        <td>{person.name}</td>
-                                        <td>{person.status}</td>
-                                        <td>{person.species}</td>
-                                        <td>{person.type}</td>
-                                        <td>{person.gender}</td>
-                                        <td>{list[index].origin["name"]}</td>
-                                        <td>{list[index].location["name"]}</td>
+                            return(<tr><td style={{display: check["id"] ? "table-cell" : "none"}}>{person.id}</td>
+                                        <td style={{display: check["image"] ? "table-cell" : "none"}}><img src={person.image} alt="" style={{width: "100px", height: "100px"}}/></td>
+                                        <td style={{display: check["name"] ? "table-cell" : "none"}}>{person.name}</td>
+                                        <td style={{display: check["status"] ? "table-cell" : "none"}}>{person.status}</td>
+                                        <td style={{display: check["species"] ? "table-cell" : "none"}}>{person.species}</td>
+                                        <td style={{display: check["type"] ? "table-cell" : "none"}}>{person.type}</td>
+                                        <td style={{display: check["gender"] ? "table-cell" : "none"}}>{person.gender}</td>
+                                        <td style={{display: check["origin"] ? "table-cell" : "none"}}>{list[index].origin["name"]}</td>
+                                        <td style={{display: check["location"] ? "table-cell" : "none"}}>{list[index].location["name"]}</td>
                                        
                                      </tr>)
                         
